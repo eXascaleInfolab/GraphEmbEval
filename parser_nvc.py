552099a42dca19c154654794366be663a2350c3c
luav <luardev@gmail.com>
2019-03-31 07:04:05 +0200

    NVC v1.2.1 implemented (valmin header attribute)

--- parser_nvc.py
+++ parser_nvc.py
@@ -14,7 +14,7 @@ import numpy as np
 
 
 def loadNvc(nvcfile):
-	"""Load network embeddings from the specified file in the NVC format v1.2
+	"""Load network embeddings from the specified file in the NVC format v1.2.1
 
 	nvcfile: str  - file name
 
@@ -31,6 +31,7 @@ def loadNvc(nvcfile):
 	ndsnum = 0  # The number of nodes
 	dimnum = 0  # The number of dimensions (reprsentative clusters)
 	rootdnum = 0  # The number of root dimensions (reprsentative clusters)
+	valmin = 0  # minimal value (bottom margin) accounted in the node vectors, affects interpretation of the encoded values (format range)
 	numbered = False
 	rootdims = None  # Indices of the root dimensions
 	dimrds = None  # Dimension density ratios relative to the possibly indirect super cluster (dimension), typically >= 1
@@ -47,7 +48,7 @@ def loadNvc(nvcfile):
 	VAL_UINT16 = 2
 	VAL_FLOAT32 = 4
 	valfmt = VAL_UINT8  # Falue format
-	hdrvals = {'nodes:': None, 'dimensions:': None, 'rootdims:': None, 'value:': None, 'compression:': None, 'numbered:': None}
+	hdrvals = {'nodes:': None, 'dimensions:': None, 'rootdims:': None, 'value:': None, 'compression:': None, 'valmin:': None, 'numbered:': None}
 	assert not any(filter(lambda k: not k.endswith(':'), hdrvals)), 'Header attribute names must end with ":"'
 	irow = 0  # Payload line (matrix row) index (of either dimensions or nodes)
 	nvec = None  # Node vectors matrix
@@ -86,6 +87,7 @@ def loadNvc(nvcfile):
 						ndsnum = np.uint32(hdrvals.get('nodes:', ndsnum))
 						dimnum = np.uint16(hdrvals.get('dimensions:', dimnum))
 						rootdnum = np.uint16(hdrvals.get('rootdims:', rootdnum))
+						valmin = np.float32(hdrvals.get('valmin:', valmin))
 						numbered = int(hdrvals.get('numbered:', numbered))  # Note: bool('0') is True (non-empty string)
 						comprstr = hdrvals.get('compression:', '').lower()
 						if comprstr == 'none':
@@ -179,7 +181,7 @@ def loadNvc(nvcfile):
 					nids, vals = zip(*[v.split(':') for v in vals])
 					if valfmt == VAL_UINT8 or valfmt == VAL_UINT16:
 						# vals = [np.float32(1. / np.uint16(v)) for v in vals]
-						vals = [np.float32(vqnorm - np.uint16(v) + 1) / vqnorm for v in vals]
+						vals = [valmin + (1 - valmin) * np.float32(vqnorm - np.uint16(v) + 1) / vqnorm for v in vals]
 					else:
 						assert valfmt == VAL_FLOAT32, 'Unexpected valfmt'
 					for i, nd in enumerate(nids):
@@ -192,7 +194,7 @@ def loadNvc(nvcfile):
 					dms, vals = zip(*[v.split(':') for v in vals])
 					if valfmt == VAL_UINT8 or valfmt == VAL_UINT16:
 						# vals = [np.float32(1. / np.uint16(v)) for v in vals]
-						vals = [np.float32(vqnorm - np.uint16(v) + 1) / vqnorm for v in vals]
+						vals = [valmin + (1 - valmin) * np.float32(vqnorm - np.uint16(v) + 1) / vqnorm for v in vals]
 					else:
 						assert valfmt == VAL_FLOAT32, 'Unexpected valfmt'
 					for i, dm in enumerate(dms):
@@ -203,7 +205,7 @@ def loadNvc(nvcfile):
 					if v[0] != '0':
 						if valfmt == VAL_UINT8 or valfmt == VAL_UINT16:
 							# nvec[irow, j + corr] = 1. / np.uint16(v)
-							nvec[irow, j + corr] = np.float32(vqnorm - np.uint16(v) + 1) / vqnorm
+							nvec[irow, j + corr] = valmin + (1 - valmin) * np.float32(vqnorm - np.uint16(v) + 1) / vqnorm
 						else:
 							assert valfmt == VAL_FLOAT32 or valfmt == VAL_BIT, 'Unexpected valfmt'
 							nvec[irow, j + corr] = v
@@ -222,7 +224,7 @@ def loadNvc(nvcfile):
 						continue
 					if valfmt == VAL_UINT8 or valfmt == VAL_UINT16:
 						# nvec[irow, j + corr] = 1. / np.uint16(v)
-						nvec[irow, j + corr] = np.float32(vqnorm - np.uint16(v) + 1) / vqnorm
+						nvec[irow, j + corr] = valmin + (1 - valmin) * np.float32(vqnorm - np.uint16(v) + 1) / vqnorm
 					else:
 						assert valfmt == VAL_FLOAT32 or valfmt == VAL_BIT, 'Unexpected valfmt'
 						nvec[irow, j + corr] = v
